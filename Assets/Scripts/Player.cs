c730cea6a209d15b0c71e5eec41d734343bb5dd8
ayaseya <ayaseya@gmail.com>
2018-04-23 15:56:30 +0900

    Added a collider to a player and comment to code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-	public float m_Speed = 6f;
+	public float m_Speed = 6f;	// 移動速度
 
 	Animator m_Animator;
 	Rigidbody m_Rigidbody;
@@ -19,9 +18,11 @@ public class Player : MonoBehaviour
 
 	void FixedUpdate ()
 	{
+		// キー入力の取得
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 
+		// キー入力からベクトルを設定する
 		m_Movement.Set (h, 0f, v);
 
 		Move ();
@@ -31,25 +32,34 @@ public class Player : MonoBehaviour
 
 	private void Move ()
 	{
+		// ベクトルから移動量を取得する
 		m_Movement = m_Movement.normalized * m_Speed * Time.deltaTime;
 		m_Rigidbody.MovePosition (transform.position + m_Movement);
 	}
 
 	private void Turn ()
 	{
+		// 移動中でなければ回転処理は行わない
 		if (!isRunning ())
+			// NOP.
 			return;
+
+		// ベクトルから回転方向を取得する
 		Quaternion targetRotation = Quaternion.LookRotation (m_Movement);
+
+		// 回転処理
 		transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, 360.0f * Time.deltaTime);
 	}
 
 	private void Animate ()
 	{
+		// 走るアニメーションの有効、無効化
 		m_Animator.SetBool ("Running", isRunning ());
 	}
 
 	private bool isRunning ()
 	{
+		// ベクトルの長さでキャラクターが移動中か判断する
 		return m_Movement.magnitude != 0f;
 	}
 }
